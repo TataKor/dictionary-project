871ffe840e846c42c854c94e2bbc55b5fd5241d7
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-25 15:00:29 +0300

    Logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
+import logo from "./logo.png";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
+        <img src={logo} className="App-logo img-fluid"
+        alt="logo" />
       </header>
     </div>
   );
 }
-
-export default App;
