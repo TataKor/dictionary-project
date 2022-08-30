a31d0d9b8fb3cc23ffec98d952cdb7ce3f952db7
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-30 12:46:02 +0300

    Distroy server error wirh axios

--- src/Results.js
+++ src/Results.js
@@ -2,21 +2,20 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-    if (props.results) {
+  if (props.results) {
     return (
-        <div className="Results">
+      <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function(meaning,index) {
-            return (
-                <div key={index}>
-                    <Meaning meaning={meaning} />
-                    </div>
-                    );
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
-        </div>
+      </div>
     );
+  } else {
+    return null;
+  }
 }
-        else {
-            return null;
-        }
-    }
